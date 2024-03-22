039ee320acd006d09a2bbec605c7f844105eb197
MarceloBr69 <marcelo.burgos200405@gmail.com>
2024-03-22 19:30:03 -0300

    Second commit

--- Actividad_Proyecto_Class/src/com/ProyectoClass/ProjectTest.java
+++ Actividad_Proyecto_Class/src/com/ProyectoClass/ProjectTest.java
@@ -5,6 +5,7 @@ public class ProjectTest {
 	public static void main(String[] args) {
         Project alex = new Project("Alex", "Hola mucho gusto!");
         alex.elevatorPitch();
+        
         Project maria = new Project("Maria");
         maria.elevatorPitch();
 	}
